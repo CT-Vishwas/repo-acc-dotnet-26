3402a0f1bff09d67367d43b4e7beb24c8ff2cac7
Vishwas K Singh <vishwas@cloudthat.com>
2026-03-31 17:04:26 +0530

    fix & completing Product Servoce
    Signed-off-by: Vishwas K Singh <vishwas@cloudthat.com>

--- InventoryApp/Inventory.Infra/Services/ProductService.cs
+++ InventoryApp/Inventory.Infra/Services/ProductService.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using Inventory.Core.DTOs.Requests;
 using Inventory.Core.DTOs.Responses;
+using Inventory.Core.Entities;
 using Inventory.Core.Interfaces;
 
 namespace Inventory.Infra.Services;
@@ -8,13 +10,18 @@ public class ProductService : IProductService
 {
     private readonly IProductRepository _productRepository;
 
-    public ProductService(IProductRepository productRepository)
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
     {
         _productRepository = productRepository;
+        _mapper = mapper;
     }
 
-    public Task<ProductResponseDTO> CreateProduct(ProductRequestDTO productRequestDTO)
+    public async Task<ProductResponseDTO> CreateProduct(ProductRequestDTO productRequestDTO)
     {
-        
+        var product = await _productRepository.AddAsync(_mapper.Map<Product>(productRequestDTO));
+
+        return _mapper.Map<ProductResponseDTO>(product);
     }
 }
